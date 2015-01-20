53999cbdd687014dec2da6cc2b567b1d7ca8145c
Prabin Timsina <prabinmetals@gmail.com>
2015-01-19 22:10:20 -0700

    Update MainActivity.java
    Generate Hask Key for your facebook app

--- src/com/prabin/facebookconnection/MainActivity.java
+++ src/com/prabin/facebookconnection/MainActivity.java
@@ -9,6 +9,23 @@ public class MainActivity extends DroidGap {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		// Generate HASH Key for your app
+		try {
+			PackageInfo info = getPackageManager().getPackageInfo(
+					"com.prabin.facebookconnection",
+					PackageManager.GET_SIGNATURES);
+			for (Signature signature : info.signatures) {
+				MessageDigest md = MessageDigest.getInstance("SHA");
+				md.update(signature.toByteArray());
+				String s = Base64.encodeToString(md.digest(), Base64.DEFAULT);
+				Log.d("KeyHash:", s);
+
+			}
+		} catch (NameNotFoundException e) {
+
+		} catch (NoSuchAlgorithmException e) {
+
 		super.loadUrl("file:///android_asset/www/index.html");
 	}
 }
